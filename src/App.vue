df9d34a761d72abd5f9cb4c97c705e91ba03b03a
Mako <maggi08mus@gmail.com>
2023-04-30 00:39:42 +0600

    format

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const startGame = () => {
+  console.log('startGame')
+}
+
+startGame()
+</script>
 
 <template>
   <div class=""></div>
